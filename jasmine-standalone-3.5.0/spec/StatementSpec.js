d0e314eed930a83d83f3592f3b57daec80508091
joannacollier <collj035@gmail.com>
2020-07-11 21:23:10 +0100

    Withdrawal and deposit printing in correct order

--- jasmine-standalone-3.5.0/spec/StatementSpec.js
+++ jasmine-standalone-3.5.0/spec/StatementSpec.js
@@ -8,8 +8,13 @@ describe("Statement", function(){
     statement = new Statement
   })
 
-  it("Prints a 'desposit'", function(){
+  it("Prints a 'deposit'", function(){
     var transactionRecordDouble = {0: {date: "11/11/2011", credit: 1000, debit: "", balance: 1000}}
     expect(statement.print(transactionRecordDouble)).toEqual(("date||credit||debit||balance\n11/11/2011||1000||||1000\n"))
+  });
+
+  it("Prints a 'deposit' and 'withdrawal'", function(){
+    var transactionRecordDouble = {0: {date: "11/11/2011", credit: 1000, debit: "", balance: 1000}, 1: {date: "12/11/2011", credit: "", debit: 500, balance: 500}}
+    expect(statement.print(transactionRecordDouble)).toEqual(("date||credit||debit||balance\n12/11/2011||||500||500\n11/11/2011||1000||||1000\n"))
   });
 });
